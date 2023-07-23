33e584c8012120ac9916a326be05347ddb4ab938
atulsaini7900 <atulsaini7900@gmail.com>
2023-07-23 16:37:22 +0530

    Update File

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** ------------------ IMPORTING PACKAGE ------------------ **/
 const express = require('express');
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 const path = require('path');
 
